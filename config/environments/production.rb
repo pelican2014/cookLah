d9ba36be386100d8e159987c79527782ebf11c49
YANG XIYA <xiya@u.nus.edu>
2016-02-25 03:40:53 +0800

    fix sendgrid (unconfirmed)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'https://cookLah.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'cookLah.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => '25',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
+  :domain         => ENV['SENDGRID_DOMAIN']
 }
 end
