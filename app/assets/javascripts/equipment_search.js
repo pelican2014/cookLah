3e3a5a010cbdd20057a9c3db5f93ffd372704948
YANG XIYA <xiya@u.nus.edu>
2016-02-27 17:55:27 +0800

    disable live search due to slow response online

--- app/assets/javascripts/equipment_search.js
+++ app/assets/javascripts/equipment_search.js
@@ -4,7 +4,7 @@ $(function() {
   ajax_search_submit('#equipment_search');
 
   // ajax call for keying into the search bar
-  ajax_search_keyup('#equipment_search')
+  // ajax_search_keyup('#equipment_search')
 
   // ajax call for submitting search form of equipment specifically for recipe
   // ajax call for submitting search form
--- app/assets/javascripts/ingredients_search.js
+++ app/assets/javascripts/ingredients_search.js
@@ -3,7 +3,7 @@ $(function() {
   ajax_search_submit('#ingredients_search');
 
   // ajax call for keying into the search bar
-  ajax_search_keyup('#ingredients_search');
+  // ajax_search_keyup('#ingredients_search');
 
   // ajax call for submitting search form of ingredients specifically for recipe
   // ajax call for submitting search form
